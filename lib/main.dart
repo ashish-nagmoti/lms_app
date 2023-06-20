621de786cbff93dca366d4f9ae7a15f29520f1ac
ashish_nagmoti <ashishnagmoti6@gmail.com>
2023-06-20 14:01:24 +0000

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:flutterapp/lms_20appapp/generatedframe2widget/GeneratedFrame2Widget.dart';
+import 'package:flutterapp/lms_20appapp/generatedframe3widget/GeneratedFrame3Widget.dart';
+import 'package:flutterapp/lms_20appapp/generatedframe4widget/GeneratedFrame4Widget.dart';
+import 'package:flutterapp/lms_20appapp/generatedframe5widget/GeneratedFrame5Widget.dart';
+import 'package:flutterapp/lms_20appapp/generatedframe6widget/GeneratedFrame6Widget.dart';
+import 'package:flutterapp/lms_20appapp/generatedframe8widget/GeneratedFrame8Widget.dart';
+import 'package:flutterapp/lms_20appapp/generatedframe9widget/GeneratedFrame9Widget.dart';
+import 'package:flutterapp/lms_20appapp/generatedframe10widget/GeneratedFrame10Widget.dart';
+import 'package:flutterapp/lms_20appapp/generatedframe4958widget/GeneratedFrame4958Widget.dart';
+import 'package:flutterapp/lms_20appapp/generatedframe4959widget/GeneratedFrame4959Widget.dart';
+import 'package:flutterapp/lms_20appapp/generatedframe4961widget/GeneratedFrame4961Widget.dart';
+import 'package:flutterapp/lms_20appapp/generatedframe4963widget/GeneratedFrame4963Widget.dart';
+import 'package:flutterapp/lms_20appapp/generatedframe4960widget/GeneratedFrame4960Widget.dart';
+import 'package:flutterapp/lms_20appapp/generatedframe4962widget/GeneratedFrame4962Widget.dart';
+import 'package:flutterapp/lms_20appapp/generatedframe4964widget/GeneratedFrame4964Widget.dart';
+import 'package:flutterapp/lms_20appapp/generatedframe4965widget/GeneratedFrame4965Widget.dart';
+import 'package:flutterapp/lms_20appapp/generatedframe4966widget/GeneratedFrame4966Widget.dart';
+import 'package:flutterapp/lms_20appapp/generatedframe4967widget/GeneratedFrame4967Widget.dart';
+import 'package:flutterapp/lms_20appapp/generatedframe4969widget/GeneratedFrame4969Widget.dart';
+import 'package:flutterapp/lms_20appapp/generatedframe4968widget/GeneratedFrame4968Widget.dart';
+import 'package:flutterapp/lms_20appapp/generatedframe4970widget/GeneratedFrame4970Widget.dart';
+import 'package:flutterapp/lms_20appapp/generatedframe4973widget/GeneratedFrame4973Widget.dart';
+import 'package:flutterapp/lms_20appapp/generatedframe4977widget/GeneratedFrame4977Widget.dart';
+import 'package:flutterapp/lms_20appapp/generatedframe4971widget/GeneratedFrame4971Widget.dart';
+
+void main() {
+  runApp(lms_20appApp());
+}
+
+class lms_20appApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      initialRoute: '/GeneratedFrame2Widget',
+      routes: {
+        '/GeneratedFrame2Widget': (context) => GeneratedFrame2Widget(),
+        '/GeneratedFrame3Widget': (context) => GeneratedFrame3Widget(),
+        '/GeneratedFrame4Widget': (context) => GeneratedFrame4Widget(),
+        '/GeneratedFrame5Widget': (context) => GeneratedFrame5Widget(),
+        '/GeneratedFrame6Widget': (context) => GeneratedFrame6Widget(),
+        '/GeneratedFrame8Widget': (context) => GeneratedFrame8Widget(),
+        '/GeneratedFrame9Widget': (context) => GeneratedFrame9Widget(),
+        '/GeneratedFrame10Widget': (context) => GeneratedFrame10Widget(),
+        '/GeneratedFrame4958Widget': (context) => GeneratedFrame4958Widget(),
+        '/GeneratedFrame4959Widget': (context) => GeneratedFrame4959Widget(),
+        '/GeneratedFrame4961Widget': (context) => GeneratedFrame4961Widget(),
+        '/GeneratedFrame4963Widget': (context) => GeneratedFrame4963Widget(),
+        '/GeneratedFrame4960Widget': (context) => GeneratedFrame4960Widget(),
+        '/GeneratedFrame4962Widget': (context) => GeneratedFrame4962Widget(),
+        '/GeneratedFrame4964Widget': (context) => GeneratedFrame4964Widget(),
+        '/GeneratedFrame4965Widget': (context) => GeneratedFrame4965Widget(),
+        '/GeneratedFrame4966Widget': (context) => GeneratedFrame4966Widget(),
+        '/GeneratedFrame4967Widget': (context) => GeneratedFrame4967Widget(),
+        '/GeneratedFrame4969Widget': (context) => GeneratedFrame4969Widget(),
+        '/GeneratedFrame4968Widget': (context) => GeneratedFrame4968Widget(),
+        '/GeneratedFrame4970Widget': (context) => GeneratedFrame4970Widget(),
+        '/GeneratedFrame4973Widget': (context) => GeneratedFrame4973Widget(),
+        '/GeneratedFrame4977Widget': (context) => GeneratedFrame4977Widget(),
+        '/GeneratedFrame4971Widget': (context) => GeneratedFrame4971Widget(),
+      },
+    );
+  }
+}
